eda4e53901ffc33623b651e19f97c6f7621cbd20
lilyammy <cooljuno456@gmail.com>
2023-10-18 12:38:03 -0500

    not completed yet

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import random
+
+from PIL import Image, ImageDraw
+from PIL.ImageDraw import ImageDraw
 from kivy.app import App
 from kivy.uix.screenmanager import Screen
+
 
 
 
@@ -8,15 +13,38 @@ class PhotoEditorApp(App):
 
 class Display(Screen):
 
-    def display_image(self):
-        return images[index]
-    def user_image(self, user_input ):
-        if user_input == "CatHalloween.jpg":
-            self.ids.image.sources=self.display_image()
+    def load_image(self):
+        self.ids.image.source= self.ids.user_input.text
 
-        self.ids.image.source= self.display_image()
+    def sepia( self, name):
+        image = self.ids.image.source
+        img = Image.open(image)
+        pixels = img.load()
+        for y in range(img.size[1]):
+            for x in range(img.size[0]):
+                red = pixels[x, y][0]
+                green = pixels[x, y][1]
+                blue = pixels[x, y][2]
+                red1 = int(red * .393 + green * 0.769 + blue * 0.189)
+                green1 = int(red * .349 + green * 0.686 + blue * 0.168)
+                blue1 = int(red * .272 + green * 0.534 + blue * 0.131)
+                pixels[x, y] = (red1, green1, blue1)
+        img.save(name + "sepia.jpg")
+        self.ids.image.source = name+ "sepia.jpg"
 
 
+    def inverse(self,name):
+        image =self.ids.image.source
+        img = Image.open(image)
+        pixels = img.load()
+        for y in range(img.size[1]):
+            for x in range(img.size[0]):
+                red = 255 - pixels[x, y][0]
+                green = 255 - pixels[x, y][1]
+                blue = 255 - pixels[x, y][2]
+                pixels[x, y] = (red, green, blue)
+        img.save(name+ "invert.jpg")
+        self.ids.image.source = name +"invert.jpg"
 
 
 
